8897b9f6498bc4f7ecca37937b5438c1eaeb7cf5
Jack Gorton <jack@Jacks-MacBook-Pro.local>
2022-06-10 12:25:51 +0100

    adding some correlation stuff

--- src/functions.py
+++ src/functions.py
@@ -41,7 +41,7 @@ def process_Ticket(data):
     
     ticket_info['prefix'].value_counts()
     ticket_info['J_prefix'] = ticket_info['prefix'].copy()
-    ticket_info['J_ticket_location'] = ''
+    ticket_info['J_ticket_location'] = 'UNKNOWN'
     ticket_info.loc[ticket_info['J_prefix'].str.contains('|'.join(['SOTON','STON'])), 'J_ticket_location'] = 'southampton'
     ticket_info.loc[ticket_info['J_prefix'].str.contains('|'.join(['PARIS'])), 'J_ticket_location'] = 'paris'
     # looks like we have people from southampton? STON SOTON 
@@ -107,8 +107,6 @@ def calculate_woe_for_column(newX, var,target = 'died', eps= 1e-10, min_rate = 0
     maxdiff = -1
     woe_trend = -1
 
-
-
     counts = newX.groupby(by=var, observed=True)[
         target].value_counts().sort_index()
 
@@ -164,3 +162,99 @@ def get_woe_trend(df):
         return -1
     else:
         return 0
+    
+class CorrelationAnalysis:
+    """ could also add cramers v for categorical data. """
+    def __init__(self,df, iv_table):
+
+        
+        self._corr_tables = {}
+        
+        self._corr_tables['pearson'] = pearson_df = df.corr(method='pearson')
+        self._corr_tables['kendall'] = pearson_df = df.corr(method='kendall')
+        self._corr_tables['spearman'] = pearson_df = df.corr(method='spearman')
+        self._iv_table= iv_table
+       
+    def _get_correlated(self, corr_type, thresh):
+        correlated = []
+        correlated_dict = {}
+        df = self._corr_tables[corr_type]
+        for i, row in df.iterrows():
+            for c, val in row.items():
+                if c == i :
+                    pass
+                else:
+                    if val> thresh:
+                        print('{} correlated with {}'.format(i,c))
+                        if i in correlated_dict.keys():
+                            correlated_dict[i].append(c)
+                        #elif c in correlated_dict.keys() and i in correlated_dict[c]:
+                        #    pass
+                        else:
+                            correlated_dict[i] = [c]
+        return correlated_dict
+    
+    @staticmethod
+    def _update_dict(base, new):
+        final = base.copy()
+        for k in new.keys():
+            if k in base.keys():
+                final[k] = list(set(base[k]+new[k]))
+            else:
+                final[k] = new[k]
+        return final
+        
+    def _get_best_by_iv(self, iv_table, var1, corr_with):
+        """find the best feature by comparing the var1 to those it is correlated with"""
+        var1_iv = iv_table.loc[var1, 'IV']
+        others = iv_table.loc[(iv_table.index.isin(corr_with)) & (iv_table['IV'] > var1_iv), :]
+        if others.empty:
+            return var1, corr_with
+        else:
+            return others.sort_values(by='IV', ascending=False).index[0], [i for i in corr_with+[var1] if i != others.sort_values(by='IV', ascending=False).index[0]]
+                                                                  
+
+    def analyse_correlations(self, pearson_thresh=0.8, spearman_thresh=0.8, kendall_thresh=0.8):
+        """keep the best feature according to iv if found to be correlated with other feats"""
+        self._thresholds = {}
+        self._thresholds['pearson'] = pearson_thresh
+        self._thresholds['spearman'] = spearman_thresh
+        self._thresholds['kendall'] = kendall_thresh
+        correlations = {}
+        all_corr = {}
+        for corr_type in ['spearman','kendall','pearson']:
+            print('running for {}'.format(corr_type))
+            corrs = self._get_correlated(corr_type,self._thresholds[corr_type] )
+            #print(corrs)
+            correlations[corr_type] = corrs
+            all_corr= self._update_dict(all_corr, corrs)
+        
+        if len(all_corr.keys())>0:
+            print(all_corr)
+            prev_rejects = []
+            final_list = []
+            for var1, corr_with in all_corr.items():
+                print('filtering for {}'.format(var1))
+                filtered_corr_with = list(set(corr_with)-set(prev_rejects))
+                if var1 in prev_rejects:
+                    pass
+                elif len(filtered_corr_with) == 0:
+                    final_list.append(var1)
+                else:
+                    selected, rejects = self._get_best_by_iv(self._iv_table, var1, corr_with)
+                    print('rejected {}'.format(rejects))
+                    #if we have rejected one that we previously accepted- 
+                    # we want to go back and maybe keep the rejects from back then (assuming they are not correlated)
+                    if len(set(rejects).intersection(final_list)):
+                        print(' we are trying to reject one that was previously accepted- do something here (does not occur for titanic data)')
+                    
+                    final_list.append(selected)
+                    prev_rejects.extend(rejects)
+                    
+        final = list(set(self._iv_table.index.to_list()) - set(prev_rejects)) + list(set(final_list))   
+        return final, prev_rejects
+    
+    
+    
+    
+        
